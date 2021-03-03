def6d206e5855b065edcbb7cc8883276238cd3d2
Joao Fortunato <joaomlfortunato@gmail.com>
2021-03-03 20:33:19 +0000

    Setup Style Files

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,12 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
