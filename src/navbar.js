011a23e2e76a9670a8d1895b79a7485e83f8be04
Joao Fortunato <joaomlfortunato@gmail.com>
2021-03-26 21:04:30 +0000

    Add Nav Bar Main Style
    Missing fonts intregation.

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,11 @@
 function createNavBar () {
     let navContainer = document.createElement('nav');
     
-    let homeDiv = createNavItem('Joao Steak House');
-    let contactsDiv = createNavItem('Contacts');
-    let aboutDiv = createNavItem('About');
-    let menuDiv = createNavItem('Menu');
-    let galleryDiv = createNavItem('Gallery');
+    let homeDiv = createNavItem('Joao Steak House', ["nav-item"]);
+    let contactsDiv = createNavItem('Contacts', ["nav-item"]);
+    let aboutDiv = createNavItem('About', ["nav-item"]);
+    let menuDiv = createNavItem('Menu', ["nav-item"]);
+    let galleryDiv = createNavItem('Gallery', ["nav-item"]);
 
     navContainer.appendChild(contactsDiv);
     navContainer.appendChild(aboutDiv);
@@ -13,14 +13,20 @@ function createNavBar () {
     navContainer.appendChild(menuDiv);
     navContainer.appendChild(galleryDiv);
 
+    navContainer.classList.add("nav-bar")
     return navContainer
 }
 
-function createNavItem (itemTextContent) {
+function createNavItem (itemTextContent, itemClasses = []) {
     let navItem = document.createElement('div');
     let itemText = document.createElement('span');
     itemText.textContent = itemTextContent;
     navItem.appendChild(itemText);
+
+    if (itemClasses.length !== 0) {
+        navItem.classList.add(...itemClasses);
+    }
+
     return navItem
 }
 
